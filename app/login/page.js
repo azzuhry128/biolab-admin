aeda594bb9730874b3a344dc117d2e9f045d59a1
muhammad rofif azzuhry <muhammadazzuhry04@gmail.com>
2024-12-11 22:23:12 +0700

    some changes

--- app/login/page.js
+++ app/login/page.js
@@ -1,13 +1,13 @@
 
 import axios from "axios";
 
-export default function Register() {
+export default function Login() {
   const localLoginURL = process.env.LOCAL_LOGIN_URL;
-  const labels = ["hospital name", "email address", "password"]
+  const labels = ["email address", "password"]
 
   console.log(localLoginURL);
 
-  const handleRegister = async (hospitalEmailInput, hospitalPasswordInput) => {
+  const handleLogin = async (hospitalEmailInput, hospitalPasswordInput) => {
     const result = await axios({
       method: "get",
       url: localLoginURL,
@@ -40,9 +40,9 @@ export default function Register() {
   return <div className="flex flex-col w-full h-screen justify-center items-center">
     <div className=" border-2 border-slate-900 rounded-md p-12">
       <InputComponent />
-      <h1 className="capitalize font-medium mt-2">already have account? Login</h1>
+      <h1 className="capitalize font-medium mt-2">cont have account? register</h1>
       <div className="w-full flex justify-end">
-        <button className="bg-slate-900 w-24 text-white rounded-md mt-12 p-2">Register</button>
+        <button className="bg-slate-900 w-24 text-white rounded-md mt-12 p-2">Login</button>
       </div>
     </div>
 </div>
